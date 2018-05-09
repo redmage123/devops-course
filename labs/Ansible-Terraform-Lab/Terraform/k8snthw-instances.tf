63dd53a44e7bd55861d1a2f7d064113004da0019
bbrelin <bbrelin@gmail.com>
2018-05-09 15:56:10 +0100

    Added certificate directory with certs

--- labs/Ansible-Terraform-Lab/Terraform/k8snthw-instances.tf
+++ labs/Ansible-Terraform-Lab/Terraform/k8snthw-instances.tf
@@ -11,3 +11,32 @@ resource "aws_instance" "etcd" {
     availability_zone = "eu-west-1a"
     vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
     key_name = "my-keypair"
+}
+
+resource "aws_instance" "worker" {
+    count = 3
+    ami = "ami-1967056a" // Unbuntu 16.04 LTS HVM, EBS-SSD
+    instance_type = "t2.micro"
+
+    subnet_id = "${aws_subnet.kubernetes.id}"
+    private_ip = "${cidrhost("10.43.0.0/16", 10 + count.index)}"
+    associate_public_ip_address = true
+
+    availability_zone = "eu-west-1a"
+    vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+    key_name = "my-keypair"
+}
+
+resource "aws_instance" "controller" {
+    count = 1
+    ami = "ami-1967056a" // Unbuntu 16.04 LTS HVM, EBS-SSD
+    instance_type = "t2.micro"
+
+    subnet_id = "${aws_subnet.kubernetes.id}"
+    private_ip = "${cidrhost("10.43.0.0/16", 10 + count.index)}"
+    associate_public_ip_address = true
+
+    availability_zone = "eu-west-1a"
+    vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+    key_name = "my-keypair"
+}
